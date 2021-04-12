39265de7ec0b95a381f1c564295988a14c64c3a7
Vaishbhalla1 <79858923+Vaishbhalla1@users.noreply.github.com>
2021-04-12 15:40:06 +0100

    Final code

--- UltrasonicTest/src/test_Ultrasonic.java
+++ UltrasonicTest/src/test_Ultrasonic.java
@@ -1,23 +1,34 @@
 import com.phidget22.*;
 public class test_Ultrasonic{  
-    public static void main(String[] args) throws Exception{  
+    protected static final int SensorValue = 0;
+
+	public static void main(String[] args) throws Exception{  
     	VoltageRatioInput voltageRatioInput1 = new VoltageRatioInput();
 
 		voltageRatioInput1.setChannel(1);
 
 		voltageRatioInput1.addSensorChangeListener(new VoltageRatioInputSensorChangeListener() {
 			public void onSensorChange(VoltageRatioInputSensorChangeEvent e) {
+				if(e.getSensorValue()<=100) {
 				System.out.println("SensorValue: " + e.getSensorValue());
 				System.out.println("SensorUnit: " + e.getSensorUnit().symbol);
-				System.out.println("----------");
+				System.out.println("close object");
+				} 				else  {
+					System.out.println("SensorValue: " + e.getSensorValue());
+					System.out.println("SensorUnit: " + e.getSensorUnit().symbol);
+					System.out.println("away object");
+					
+				}
+				
 			}
+			
 		});
 
 		voltageRatioInput1.open(5000);
 
 		voltageRatioInput1.setSensorType(VoltageRatioSensorType.PN_1128);
 
-		//Wait until Enter has been pressed before exiting
+	
 		System.in.read();
 
 		voltageRatioInput1.close();
