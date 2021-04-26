4d93d69a25b6e08de2efca985bcf1331a12084ea
Adam Cook <adamthelurker@gmail.com>
2021-04-26 13:41:47 +0100

    Compasss

---
+++ compass-master/app/src/main/java/com/sevencrayons/compass/CompassActivity.java
@@ -0,0 +1,128 @@
+package com.sevencrayons.compass;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class CompassActivity extends AppCompatActivity {
+
+    private static final String TAG = "CompassActivity";
+
+    private Compass compass;
+    private ImageView arrowView;
+    private TextView sotwLabel;  // SOTW is for "side of the world"
+
+    private float currentAzimuth;
+    private SOTWFormatter sotwFormatter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compass);
+
+        sotwFormatter = new SOTWFormatter(this);
+
+        arrowView = findViewById(R.id.main_image_hands);
+        sotwLabel = findViewById(R.id.sotw_label);
+        setupCompass();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "start compass");
+        compass.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        compass.stop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        compass.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "stop compass");
+        compass.stop();
+    }
+
+    private void setupCompass() {
+        compass = new Compass(this);
+        Compass.CompassListener cl = getCompassListener();
+        compass.setListener(cl);
+    }
+
+    private void adjustArrow(float azimuth) {
+        Log.d(TAG, "will set rotation from " + currentAzimuth + " to "
+                + azimuth);
+
+        Animation an = new RotateAnimation(-currentAzimuth, -azimuth,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+        currentAzimuth = azimuth;
+
+        an.setDuration(500);
+        an.setRepeatCount(0);
+        an.setFillAfter(true);
+
+        arrowView.startAnimation(an);
+    }
+
+    private float bearing(double lat1, double lon1, double lat2, double lon2){
+        double longitude1 = lon1;
+        double longitude2 = lon2;
+        double latitude1 = Math.toRadians(lat1);
+        double latitude2 = Math.toRadians(lat2);
+        double longDiff= Math.toRadians(longitude2-longitude1);
+        double y= Math.sin(longDiff)*Math.cos(latitude2);
+        double x=Math.cos(latitude1)*Math.sin(latitude2)-Math.sin(latitude1)*Math.cos(latitude2)*Math.cos(longDiff);
+
+        return (float)(Math.toDegrees(Math.atan2(y, x))+360)%360;
+    }
+
+    private char getDirection(float azimuth, float bearing){
+        final char[] direction = {'U', 'R', 'D', 'L'};
+        final String bearingChar = "NESW";
+        int azimuthVal = bearingChar.indexOf(sotwFormatter.format(azimuth));
+        int bearingVal = bearingChar.indexOf(sotwFormatter.format(bearing));
+        int dirVal = azimuthVal - bearingVal;
+        if (dirVal < 0) {
+            dirVal = 4 - dirVal;
+        }
+
+        return direction[dirVal];
+    }
+
+    private void adjustSotwLabel(float azimuth) {
+        sotwLabel.setText(sotwFormatter.format(azimuth));
+    }
+
+    private Compass.CompassListener getCompassListener() {
+        return new Compass.CompassListener() {
+            @Override
+            public void onNewAzimuth(final float azimuth) {
+                // UI updates only in UI thread
+                // https://stackoverflow.com/q/11140285/444966
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adjustArrow(azimuth);
+                        adjustSotwLabel(azimuth);
+                    }
+                });
+            }
+        };
+    }
+}
